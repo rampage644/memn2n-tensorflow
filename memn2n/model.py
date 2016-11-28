6d9cab9cbe04a9860609afddde6d241264c7d230
Sergei Turukin <rampage644@gmail.com>
2016-11-28 14:31:19 +0300

    Visualize results and training process

--- memn2n/model.py
+++ memn2n/model.py
@@ -1,3 +1,4 @@
+
 '''Model'''
 from __future__ import absolute_import
 from __future__ import division
@@ -10,7 +11,8 @@ import tensorflow as tf
 class MemN2N(object):
     '''End-to-End model'''
 
-    def __init__(self, vocab_size, embedding_size, sentence_length, memory_size):
+    def __init__(self, lr, vocab_size, embedding_size, sentence_length, memory_size):
+        self.lr = lr
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.sentence_length = sentence_length
@@ -20,20 +22,50 @@ class MemN2N(object):
         self._create_inference()
         self._create_loss()
         self._create_train()
+        self._create_accuracy()
+
+        self.summary_op = tf.merge_all_summaries()
 
     def _create_variables(self):
+        self.global_step = tf.Variable(
+            initial_value=0,
+            name="global_step",
+            trainable=False,
+            collections=[tf.GraphKeys.GLOBAL_STEP, tf.GraphKeys.VARIABLES]
+        )
+
         with tf.variable_scope('input'):
             # sentences - stories - facts
             self.x = tf.placeholder(tf.int32, [None, self.memory_size, self.sentence_length], name='facts')
             self.q = tf.placeholder(tf.int32, [None, self.sentence_length], name='query')
             self.a = tf.placeholder(tf.int32, [None], name='answer')
 
-        with tf.variable_scope('embeddings'):
-            self.A = tf.get_variable('A', [self.vocab_size, self.embedding_size], tf.float32)
-            self.B = tf.get_variable('B', [self.vocab_size, self.embedding_size], tf.float32)
-            self.C = tf.get_variable('C', [self.vocab_size, self.embedding_size], tf.float32)
+        with tf.variable_scope('embeddings', initializer=tf.random_normal_initializer(stddev=.1)):
+            # +1 is for fixed zero input
+            zero_embedding = tf.zeros([1, self.embedding_size])
 
-            self.W = tf.get_variable('W', [self.embedding_size, self.vocab_size], tf.float32)
+            self.A = tf.concat(0, [
+                zero_embedding,
+                tf.get_variable('A', [self.vocab_size, self.embedding_size], tf.float32),
+            ])
+            self.B = tf.concat(0, [
+                zero_embedding,
+                tf.get_variable('B', [self.vocab_size, self.embedding_size], tf.float32)
+            ])
+            self.C = tf.concat(0, [
+                zero_embedding,
+                tf.get_variable('C', [self.vocab_size, self.embedding_size], tf.float32)
+            ])
+
+            self.W = tf.concat(1, [
+                tf.get_variable('W', [self.embedding_size, self.vocab_size], tf.float32),
+                tf.transpose(zero_embedding)
+            ])
+
+            tf.histogram_summary('A', self.A)
+            tf.histogram_summary('B', self.B)
+            tf.histogram_summary('C', self.C)
+            tf.histogram_summary('W', self.W)
 
     def _create_inference(self):
         with tf.variable_scope('model'):
@@ -47,14 +79,26 @@ class MemN2N(object):
             self.o = tf.reduce_sum(tf.mul(self.memory_output, self.softmax_probs, name='p-c_i'), reduction_indices=[1], name='o')
             self.logits = tf.matmul(tf.squeeze(self.u) + self.o, self.W, name='logits')
 
-            self.predicted = tf.argmax(self.logits, 1)
+            self.predicted = tf.argmax(tf.nn.softmax(self.logits), 1)
 
 
     def _create_loss(self):
         with tf.variable_scope('loss'):
-            self.a_one_hot = tf.one_hot(self.a, self.vocab_size, name='answer_one_hot')
-            self.loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(self.logits, self.a_one_hot, name='loss'))
+            a_one_hot = tf.one_hot(self.a, self.vocab_size + 1, name='answer_one_hot')
+            self.loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(self.logits, a_one_hot, name='loss'))
+
+            tf.scalar_summary('Loss', self.loss)
+
+    def _create_accuracy(self):
+        with tf.variable_scope('accuracy'):
+            correct = tf.equal(tf.cast(self.predicted, tf.int32), self.a)
+            self.accuracy = 100.0 * tf.reduce_mean(tf.cast(correct, tf.float32))
+
+            tf.scalar_summary('accuracy', self.accuracy)
 
     def _create_train(self):
-        with tf.variable_scope('traing'):
-            self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
+        with tf.variable_scope('training'):
+            train_op = tf.train.AdamOptimizer(self.lr)
+            gvs = train_op.compute_gradients(self.loss)
+            capped_gvs = [(tf.clip_by_norm(grad, 40.0), var) for grad, var in gvs]
+            self.train_op = train_op.apply_gradients(capped_gvs, global_step=self.global_step)
--- train.py
+++ train.py
@@ -7,50 +7,73 @@ from __future__ import unicode_literals
 import os
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 import memn2n.model
 import memn2n.util
 
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('embedding_size', 50, 'Dimension of word embedding')
-tf.app.flags.DEFINE_integer('sentence_length', 20, 'Sentence length')
-tf.app.flags.DEFINE_integer('memory_size', 50, 'Max memory size')
+tf.app.flags.DEFINE_integer('embedding_size', 20, 'Dimension of word embedding')
+tf.app.flags.DEFINE_integer('sentence_length', 10, 'Sentence length')
+tf.app.flags.DEFINE_integer('memory_size', 30, 'Max memory size')
 tf.app.flags.DEFINE_integer('task_id', 1, 'Task id to train')
 tf.app.flags.DEFINE_integer('epoch', 1, 'Epoch number')
 tf.app.flags.DEFINE_integer('batch_size', 32, 'Batch size')
+tf.app.flags.DEFINE_float('learning_rate', 0.001, 'Learning rate')
 tf.app.flags.DEFINE_string('train_dir', os.getcwd(), 'Directory with training files')
+tf.app.flags.DEFINE_string('log_dir', os.getcwd(), 'Directory for tensorboard logs')
+
+
+plt.style.use('fivethirtyeight')
 
 
 def main(argv=None):
-    word2idx = memn2n.util.create_vocabulary(FLAGS.train_dir)
-    idx2word = {v: k for k,v in word2idx.items()}
+    word2idx, idx2word = memn2n.util.load_vocabulary(FLAGS.train_dir)
     train, test = memn2n.util.load_dataset_for(FLAGS.task_id, FLAGS.train_dir)
 
     mem_train, query_train, answer_train = memn2n.util.vectorize_dataset(list(train), word2idx, FLAGS.memory_size, FLAGS.sentence_length)
     mem_test, query_test, answer_test = memn2n.util.vectorize_dataset(list(test), word2idx, FLAGS.memory_size, FLAGS.sentence_length)
 
     with tf.Session() as sess:
-        model = memn2n.model.MemN2N(len(word2idx), FLAGS.embedding_size, FLAGS.sentence_length, FLAGS.memory_size)
+        model = memn2n.model.MemN2N(
+            FLAGS.learning_rate, len(word2idx), FLAGS.embedding_size,
+            FLAGS.sentence_length, FLAGS.memory_size
+        )
+        writer = tf.train.SummaryWriter(FLAGS.log_dir, graph=tf.get_default_graph())
 
         sess.run(tf.initialize_all_variables())
+        loss_history = []
+        accuracy_history = []
+
         for e in range(FLAGS.epoch):
             for step in range(0, len(mem_train), FLAGS.batch_size):
                 start, end = step, step+FLAGS.batch_size if step + FLAGS.batch_size < len(mem_train) else None
-                loss, predicted, _ = sess.run([model.loss, model.predicted, model.train_op], {
+                loss, predicted, summary, _ = sess.run([model.loss, model.predicted, model.summary_op, model.train_op], {
                     model.x: mem_train[start:end],
                     model.q: query_train[start:end],
                     model.a: answer_train[start:end]
                 })
 
-                print('loss={}'.format(loss))
+                loss_history.append(loss)
+                writer.add_summary(summary)
 
-            predicted = sess.run(model.predicted, {
-                model.x: mem_test,
-                model.q: query_test
-            })
+            accuracy_history.append(np.array([
+                sess.run(model.accuracy, {model.x: mem_train, model.q: query_train, model.a: answer_train}),
+                sess.run(model.accuracy, {model.x: mem_test, model.q: query_test, model.a: answer_test})
+            ]))
 
-            print('accuracy={}'.format(100.0 * (predicted == answer_test).astype(np.int32).sum() / len(predicted)))
+            print('\rEpoch: {}/{}'.format(e+1, FLAGS.epoch), end='')
+
+        accuracy_history = np.asarray(accuracy_history)
+        _, (ax1, ax2) = plt.subplots(2, 1)
+        ax1.set_title('Loss')
+        ax1.plot(loss_history)
+        ax1.plot(np.r_[loss_history[:20], memn2n.util.moving_average(loss_history, n=20)])
+        ax2.set_title('Accuracy')
+        ax2.plot(accuracy_history[:, 0])
+        ax2.plot(accuracy_history[:, 1])
+        plt.show()
 
 if __name__ == '__main__':
     tf.app.run()
